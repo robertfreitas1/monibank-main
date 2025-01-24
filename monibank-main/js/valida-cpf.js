02db93699bf3ee89b6099c2a8ff3dadd7c53cce0
robertfreitas1 <168023572+robertfreitas1@users.noreply.github.com>
2025-01-24 11:00:34 -0300

    validando os ultimos digitos com funcitions e repetições

--- monibank-main/js/valida-cpf.js
+++ monibank-main/js/valida-cpf.js
@@ -2,7 +2,12 @@ export default function  ehUmCPF(campo){
     const cpf = campo.value.replace(/\.-/g, "")
   validaNumerosRepetidos(cpf) 
 
-  console.log( validaNumerosRepetidos(cpf) )
+  if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito (cpf)){
+    console.log("Esse CPF não existe")
+
+  }  else {
+    console.log("Existe!")
+  }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -21,3 +26,43 @@ function validaNumerosRepetidos(cpf) {
 
     return numerosRepetidos.includes(cpf)
 }
+
+function validaPrimeiroDigito(cpf){
+  let soma = 0
+  let multiplicador = 10 
+
+  for (let tamanho = 0; tamanho < 9; tamanho++ ){
+    soma += cpf [tamanho] * multiplicador;
+    multiplicador--
+  }
+
+  soma = (soma * 10 ) % 11
+  
+
+  if (soma == 10 || soma  == 11){
+    soma = 0 
+  }
+
+  return soma != cpf [9]
+}
+
+
+function validaSegundoDigito(cpf){
+  let soma = 0
+  let multiplicador = 11
+
+  for (let tamanho = 0; tamanho < 10; tamanho++ ){
+    soma += cpf [tamanho] * multiplicador;
+    multiplicador--
+  }
+
+  soma = (soma * 10 ) % 11
+  
+
+  if (soma == 10 || soma  == 11){
+    soma = 0 
+  }
+
+  return soma != cpf [10]
+}
+
